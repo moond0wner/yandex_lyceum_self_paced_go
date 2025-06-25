4ea39566e58e33363397000d6f05f1ac5e30283b
ozern <ozenov07@bk.ru>
2025-06-25 12:53:13 +0300

    Изменение README

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"slices"
+)
+
+func SortNames(names []string) {
+	slices.SortFunc(names, func(a, b string) int {
+		if a < b {
+			return -1
+		}
+		if a > b {
+			return 1
+		}
+		return 0
+	})
+}
